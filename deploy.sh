79e9397d59bfa9873488087ac40481ed563875c0
Ryuji Gushiken <gushiken@indigo.co.jp>
2015-09-01 17:33:00 +0900

    deploy tools

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#! /bin/bash -x
+
+set -ex
+
+VERSION="develop-"`date '+%Y%m%d-%H%M'`
+
+# Create new Elastic Beanstalk version
+BUCKET=gushry-spa
+
+aws --version
+aws configure set aws_access_key_id $AWSKEY
+aws configure set aws_secret_access_key $AWSSECRETKEY
+#aws configure set default.region ap-northeast-1
+#aws configure set default.output json
+
+aws s3 sync . s3://$BUCKET/ --exclude README.md --exclude circle.yml --exclude .git --exclude deploy.sh
+aws s3 ls s3://$BUCKET
+
