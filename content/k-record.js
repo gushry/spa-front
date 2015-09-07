9ffdfce5f4bdbd75a058ed20868bce1c19a3b700
Ryuji Gushiken <gushiken@indigo.co.jp>
2015-09-07 17:28:51 +0900

    個別のDELETEを実装

--- content/k-record.js
+++ content/k-record.js
@@ -1,11 +1,29 @@
-﻿var recordAPI="https://tkxssqi21g.execute-api.us-west-2.amazonaws.com/prod/record";
-
+﻿var recordAPI='https://tkxssqi21g.execute-api.us-west-2.amazonaws.com/prod/record';
+var pollInterval=2000;
 
 var Record = React.createClass({
+  handleDelete: function(e) {
+    e.preventDefault();
+    var key = React.findDOMNode(this.refs.id).id.trim();
+    $.ajax({
+      url: recordAPI+"/"+key,
+      dataType: 'json',
+      type: 'DELETE',
+//      data: JSON.stringify(d),
+      success: function(data) {
+        console.log("DELETE result:" + data);
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(recordAPI, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function() {
     return (
       <div className="record">
-        {this.props.create_time}: {this.props.name} / {this.props.price} / {this.props.at}
+        <input type="button" id={this.props.id + "|" + this.props.create_time} ref="id" value="Delete"
+               onClick={this.handleDelete} />
+        {this.props.create_time} @{this.props.at} / {this.props.name} / {this.props.price}
       </div>
     );
   }
@@ -14,8 +32,7 @@ var Record = React.createClass({
 var RecordList = React.createClass({
   render: function() {
     var data = this.props.data.Items
-
-    if (typeof data === "undefined") {
+    if (!data) {
        // initial
        return (
           <div className="recordList"></div>
@@ -23,7 +40,7 @@ var RecordList = React.createClass({
     }
     var recordNodes = data.map(function (r) {
       return (
-        <Record name={r.name} price={r.price} at={r.at} create_time={r.create_time}></Record>
+        <Record key={r.id} id={r.id} name={r.name} price={r.price} at={r.at} create_time={r.create_time} />
       );
     });
     return (
@@ -52,9 +69,9 @@ var RecordForm = React.createClass({
   render: function() {
     return (
       <form className="recordForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Where?" ref="at" />
         <input type="text" placeholder="What?" ref="name" />
         <input type="text" placeholder="How much?" ref="price" />
-        <input type="text" placeholder="Where?" ref="at" />
         <input type="submit" value="Post" />
       </form>
     );
@@ -115,7 +132,7 @@ var RecordBox = React.createClass({
 });
 
 React.render(
-  <RecordBox url={recordAPI} pollInterval={2000} />,
+  <RecordBox url={recordAPI} pollInterval={pollInterval} />,
   document.getElementById('recordArea')
 );
 
